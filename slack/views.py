12f0beb7eb144c691a80bbc66fd05e5b8882ddc5
hongsa <hsasung@gmail.com>
2016-01-21 17:47:45 +0900

    회원가입 완료

--- slack/views.py
+++ slack/views.py
@@ -1,14 +1,36 @@
+from django.contrib.auth.hashers import make_password,check_password
 from slack.models import Slack,User,Register
 from slack.serializers import SlackSerializer,UserSerializer,RegisterSerializer
 from rest_framework import generics
 from rest_framework import permissions
 import logging
+import hashlib
 logger = logging.getLogger(__name__)
+
+
+class UserSignup(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        password = make_password(self.request.data.get('password'))
+        serializer.save(password=password)
+
+class UserLogin(generics.GenericAPIView):
+
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SlackList(generics.ListCreateAPIView):
     queryset = Slack.objects.all()
     serializer_class = SlackSerializer
+
     #User외래키 값 입력을 위한 오버라이딩 메소드
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -18,14 +40,6 @@ class SlackDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SlackSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
 class RegisterList(generics.ListCreateAPIView):
     queryset = Register.objects.all()
     serializer_class = RegisterSerializer
@@ -33,5 +47,5 @@ class RegisterList(generics.ListCreateAPIView):
     #user, slack 외래키 값 입력을 위한 오버라이딩 메소드
     def perform_create(self, serializer):
         # requeset.POST or request.data ??
-        queryset = Slack.objects.get(self.request.POST.get('slack'))
+        queryset = Slack.objects.get(self.request.data.get('slack'))
         serializer.save(user=self.request.user,slack=queryset)
